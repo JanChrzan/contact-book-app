090e9d145f363310e7487326760dabefa3064adc
chrzan <jan.sebastian.chrzan@gmail.com>
2023-04-18 22:33:41 +0200

    add: babel library for unit test support

--- jest.config.react.cjs
+++ jest.config.react.cjs
@@ -3,12 +3,11 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['<rootDir>/src/tests/react/**/*.test.[jt]s?(x)'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/react/fileMock.ts",
     "\\.css$": "<rootDir>/src/tests/react/fileMock.ts"
-  }
+  },
 };
---
+++ src/tests/react/NavbarButton.test.tsx
@@ -0,0 +1,26 @@
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render } from "@testing-library/react";
+import React from "react";
+import NavbarButton from "../../components/minor/NavbarButton";
+
+describe("NavbarButton", () => {
+  it("calls onClick when clicked", () => {
+    const onClick = jest.fn();
+    const text = "Test Button";
+    const { getByText } = render(
+      <NavbarButton text={text} onClick={onClick} />
+    );
+    fireEvent.click(getByText(text));
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it("calls setState when clicked", () => {
+    const setState = jest.fn();
+    const text = "Test Button";
+    const { getByText } = render(
+      <NavbarButton text={text} setState={setState} />
+    );
+    fireEvent.click(getByText(text));
+    expect(setState).toHaveBeenCalled();
+  });
+});
